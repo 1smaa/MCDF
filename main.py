d6bbb9546cdc4ef801358833051c4ca8cce88d23
unknown <dthecooler@gmail.com>
2025-05-25 20:12:09 +0200

    Pulizia e Commento

--- main.py
+++ main.py
@@ -22,38 +22,42 @@ def orthogonalize(psi, previous_states):
     return psi
 
 
-def energy_functional(psi: np.ndarray,H: LinearOperator,states: list[np.ndarray]) -> np.ndarray:
+def energy_functional(psi: np.ndarray,H: np.ndarray,states: list[np.ndarray]) -> np.ndarray:
+    '''
+    Funzionale dell'energia da minimizzare per la ricerca degli autovalori
+    '''
     psi=orthogonalize(psi,states)
     energy=np.vdot(psi, H.dot(psi))
     return energy
 
 def main() -> None:
-    states=[]
-    h=Operators.H(N,H,Operators.V)
+    states=[] #Record degli stati passati (per essere ortogonali agli stati precedenti)
+    h=Operators.H(N,H,Operators.V) #Operatore Hamiltoniano
     #vals, vecs = eigsh(h, k=5, which='SM')
     #print(f"Double check eigenvalues: {vals}")
-    for i in range(EIGEN_N):
-        psi_start = np.random.rand(N**2)
-        psi_start /= np.linalg.norm(psi_start)
-        def matvec(psi):
-            return h.dot(psi)
-        H_op = LinearOperator((N**2,N**2), matvec=matvec)
-        sol=minimize(lambda x:energy_functional(x,H_op,states),psi_start,method="CG",options={"disp":False})
-        minimum=orthogonalize(sol.x,states)
-        #if len(states): print(f"Overlap {max([np.vdot(minimum,state) for state in states])}")
+    for i in range(EIGEN_N): #Per i primi EIGEN_N autovalori richiesti
+        psi_start = np.random.rand(N**2) #Crea uno stato random da cui partire
+        psi_start /= np.linalg.norm(psi_start) #Normalizzalo
+        sol=minimize(lambda x:energy_functional(x,h,states), #funzione da minimizzare (si può fare anche passando gli argomenti, forse più ottimale in stack size, da vedere)
+                     x0=psi_start, #Stato di partenza
+                     method="CG", #Metodo del Gradiente Coniugato
+                     options={"disp":False} #Non mostrare le statistiche di ottimizzazione (output più pulito)
+                    ) 
+        minimum=orthogonalize(sol.x,states) #Ortogonalizza lo stato finale rispetto ai precedenti
+        #if len(states): print(f"Overlap {max([np.vdot(minimum,state) for state in states])}") #Pezzo tolto per verificare che effettivamente fosse ortogonale
         #else: pass
-        states.append(minimum)
-        energy=np.vdot(minimum.conj(),h.dot(minimum))
+        states.append(minimum) #Aggiungi al record di stati passati quello calcolato
+        energy=np.vdot(minimum.conj(),h.dot(minimum)) #Calcola autoenergia associata a questo autostato
         fig=plt.figure()
-        ax=fig.add_subplot(111,projection='3d')
+        ax=fig.add_subplot(111,projection='3d') #Impostazioni di plot
         plt.title(f"Stato {i+1}, Autoenergia > {energy}")
-        x,y=np.linspace(0,L,N),np.linspace(0,L,N)
-        X,Y=np.meshgrid(x,y)
-        psi_matrix=np.zeros(shape=(N,N))
-        for i,value in enumerate(minimum):
+        x,y=np.linspace(0,L,N),np.linspace(0,L,N) #Spazio delle coordinate
+        X,Y=np.meshgrid(x,y) #Creazione della grid di base per il plot 3d
+        psi_matrix=np.zeros(shape=(N,N)) #Creazione della matrice di base dello stato, che è stato calcolato precedentemente in modo appiattito
+        for i,value in enumerate(minimum): #Trasformazione dell'autostato da vettore a matrice sulle coordinate
             x_p,y_p=Operators.l_to_coord(N,i)
             psi_matrix[x_p][y_p]=np.abs(value)**2
-        ax.plot_surface(X,Y,psi_matrix.T,cmap="viridis")
+        ax.plot_surface(X,Y,psi_matrix.T,cmap="viridis") #Plot dell'autostato
         plt.show()
         
 
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
 import numpy as np
 from typing import Callable
-from scipy.sparse import lil_matrix, csr_matrix
 
 
 class Operators(object):
     @staticmethod
     def V(x: float,y: float,h: float) -> float:
+        '''
+        Definisce il potenziale utilizzato per i calcoli, in questo caso a gradino.
+        '''
         x,y=x*h,y*h
         L=5
         if x<L: return 10
@@ -13,27 +15,32 @@ class Operators(object):
     
     @staticmethod
     def coord_to_l(n: int,x: int,y: int) -> int:
+        '''
+        Trasformazione da coordinate a indice all'interno dell'autostato appiattito.
+        '''
         return n*y+x
     
     @staticmethod
     def l_to_coord(n: int,l: int) -> tuple:
+        '''
+        Trasformazione da indice all'interno dell'autostato appiattito a coordinate.
+        '''
         y=l//n
         x=l%n
         return x,y
     
     @staticmethod
     def H(n: int,h: float,V: Callable) -> np.ndarray:
-        size=n**2
-        hamil=np.zeros(shape=(size,size))
-        hsq=h**2
+        '''
+        Restituisce la matrice (operatore) Hamiltoniana associata al problema
+        '''
+        size=n**2 #Dimensione del vettore appiattito
+        hamil=np.zeros(shape=(size,size)) #Inizializzazione della matrice
+        hsq=h**2 #Precalcolato per calcoli più efficienti
         for i in range(0,n**2):
-            hamil[i][i]=(4/hsq+V(*Operators.l_to_coord(n,i),h))
-            hamil[i][(i-1)%size]=-1/hsq
+            hamil[i][i]=(4/hsq+V(*Operators.l_to_coord(n,i),h)) #Sulla diagonale, potenziale calcolato in base ovviamente alla posizione
+            hamil[i][(i-1)%size]=-1/hsq #Condizioni periodiche sulle x
             hamil[i][(i+1)%size]=-1/hsq
-            hamil[i][(i+n)%size]=-1/hsq
+            hamil[i][(i+n)%size]=-1/hsq #Condizioni periodiche sulle y
             hamil[i][(i-n)%size]=-1/hsq
         return hamil
-
-    @staticmethod
-    def G(H: np.ndarray,lam: float) -> np.ndarray:
-        return -Operators.A(H,lam)
