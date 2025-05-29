e5f76c975832144e556db0a4a43aef2fa0843d6e
unknown <dthecooler@gmail.com>
2025-05-29 10:39:48 +0200

    Rectangular Domain

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 # Alberto ha l'oscilloscopio
 
 from src.utils import Operators
-N=20
-
-L=40
-H=L/N
+NX=20
+NY=20
+LX=40
+LY=160
+HX=LX/NX
+HY=LY/NY
 EIGEN_N=5
 
 def orthogonalize(psi, previous_states): 
@@ -31,11 +33,11 @@ def energy_functional(psi: np.ndarray,H: np.ndarray,states: list[np.ndarray]) ->
 
 def main() -> None:
     states=[] #Record degli stati passati (per essere ortogonali agli stati precedenti)
-    h=Operators.H(N,H,Operators.V) #Operatore Hamiltoniano
+    h=Operators.H(NX,NY,HX,HY,Operators.V) #Operatore Hamiltoniano
     #vals, vecs = eigsh(h, k=5, which='SM')
     #print(f"Double check eigenvalues: {vals}")
     for i in range(EIGEN_N): #Per i primi EIGEN_N autovalori richiesti
-        psi_start = np.random.rand(N**2) #Crea uno stato random da cui partire
+        psi_start = np.random.rand(NX*NY) #Crea uno stato random da cui partire
         psi_start /= np.linalg.norm(psi_start) #Normalizzalo
         sol=minimize(lambda x:energy_functional(x,h,states), #funzione da minimizzare (si può fare anche passando gli argomenti, forse più ottimale in stack size, da vedere)
                      x0=psi_start, #Stato di partenza
@@ -50,11 +52,11 @@ def main() -> None:
         fig=plt.figure()
         ax=fig.add_subplot(111,projection='3d') #Impostazioni di plot
         plt.title(f"Stato {i+1}, Autoenergia > {energy}")
-        x,y=np.linspace(0,L,N),np.linspace(0,L,N) #Spazio delle coordinate
+        x,y=np.linspace(0,LX,NX),np.linspace(0,LY,NY) #Spazio delle coordinate
         X,Y=np.meshgrid(x,y) #Creazione della grid di base per il plot 3d
-        psi_matrix=np.zeros(shape=(N,N)) #Creazione della matrice di base dello stato, che è stato calcolato precedentemente in modo appiattito
+        psi_matrix=np.zeros(shape=(NX,NY)) #Creazione della matrice di base dello stato, che è stato calcolato precedentemente in modo appiattito
         for i,value in enumerate(minimum): #Trasformazione dell'autostato da vettore a matrice sulle coordinate
-            x_p,y_p=Operators.l_to_coord(N,i)
+            x_p,y_p=Operators.l_to_coord(NX,NY,i)
             psi_matrix[x_p][y_p]=np.abs(value)**2
         ax.plot_surface(X,Y,psi_matrix.T,cmap="viridis") #Plot dell'autostato
         plt.show()
--- src/utils.py
+++ src/utils.py
@@ -4,11 +4,11 @@ from typing import Callable
 
 class Operators(object):
     @staticmethod
-    def V(x: float,y: float,h: float) -> float:
+    def V(x: float,y: float,hx: float,hy: float) -> float:
         '''
         Definisce il potenziale utilizzato per i calcoli, in questo caso a gradino.
         '''
-        x,y=x*h,y*h
+        x,y=x*hx,y*hy
         L=5
         if x<L: return 10
         else: return 0
@@ -21,26 +21,27 @@ class Operators(object):
         return n*y+x
     
     @staticmethod
-    def l_to_coord(n: int,l: int) -> tuple:
+    def l_to_coord(nx: int,ny: int,l: int) -> tuple:
         '''
         Trasformazione da indice all'interno dell'autostato appiattito a coordinate.
         '''
-        y=l//n
-        x=l%n
+        y=l//nx
+        x=l%nx
         return x,y
     
     @staticmethod
-    def H(n: int,h: float,V: Callable) -> np.ndarray:
+    def H(nx: int,ny: int,hx: float,hy:float, V: Callable) -> np.ndarray:
         '''
         Restituisce la matrice (operatore) Hamiltoniana associata al problema
         '''
-        size=n**2 #Dimensione del vettore appiattito
+        size=nx*ny #Dimensione del vettore appiattito
         hamil=np.zeros(shape=(size,size)) #Inizializzazione della matrice
-        hsq=h**2 #Precalcolato per calcoli più efficienti
-        for i in range(0,n**2):
-            hamil[i][i]=(4/hsq+V(*Operators.l_to_coord(n,i),h)) #Sulla diagonale, potenziale calcolato in base ovviamente alla posizione
-            hamil[i][(i-1)%size]=-1/hsq #Condizioni periodiche sulle x
-            hamil[i][(i+1)%size]=-1/hsq
-            hamil[i][(i+n)%size]=-1/hsq #Condizioni periodiche sulle y
-            hamil[i][(i-n)%size]=-1/hsq
+        hsqx=hx**2 #Precalcolato per calcoli più efficienti
+        hsqy=hy**2
+        for i in range(0,size):
+            hamil[i][i]=(2/hsqx+2/hsqy+V(*Operators.l_to_coord(nx,ny,i),hx,hy)) #Sulla diagonale, potenziale calcolato in base ovviamente alla posizione
+            hamil[i][(i-1)%size]=-1/hsqx #Condizioni periodiche sulle x
+            hamil[i][(i+1)%size]=-1/hsqx
+            hamil[i][(i+nx)%size]=-1/hsqy #Condizioni periodiche sulle y
+            hamil[i][(i-nx)%size]=-1/hsqy
         return hamil
